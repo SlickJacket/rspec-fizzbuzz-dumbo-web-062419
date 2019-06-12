decd8881064d145a98e83ef010d3f59826289779
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-12 02:52:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(num)
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
-  elsif num % 2 == 0
-    return nil
-  else
+  elsif (num % 3 == 0) = (num % 5 == 0)
     return "FizzBuzz"
+  else
+    return nil
   end
 end
