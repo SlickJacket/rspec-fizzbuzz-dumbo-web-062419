f72df9e15ab17719e40142cb6a41f294ef3c02ad
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-12 03:18:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if (num % 3 == 0) != (num % 5 == 0)
+  if (num % 5 == 0) != (num % 3 == 0)
     return "Fizz"
-  elsif (num % 5 == 0) || (num % 5 == 0)
+  elsif (num % 3 == 0) != (num % 5 == 0)
     return "Buzz"
   elsif num % 2 == 0
     return nil
   else
-    return "Buzz"
+    return "FizzBuzz"
   end
 end
